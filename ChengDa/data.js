a6d1f1b2d737997d6fb0fe800d0f086b99a5856a
rakukusagi <denny19tw@gmail.com>
2024-03-27 14:33:44 +0800

    p

---
+++ ChengDa/data.js
@@ -0,0 +1,422 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "萬壽路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0786719940114473,
+          "pitch": -0.07968502249887877,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.3942067566047704,
+          "pitch": 0.04708377737148162,
+          "rotation": 10.995574287564278,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.07357635032127519,
+          "pitch": 0.02904664566855608,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.0903658245972494,
+          "pitch": -0.01870511839310751,
+          "rotation": 5.497787143782138,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8855062127975764,
+          "pitch": 0.09771223140716678,
+          "title": "政大一 公車站",
+          "text": "等公車..."
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "指南路二段（往道南橋）",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1258210980233008,
+          "pitch": 0.038711432496258524,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.07559470685771963,
+          "pitch": -0.014895799608730442,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4844899681461392,
+          "pitch": -0.07756391166056886,
+          "title": "小木屋鬆餅 政治大學店",
+          "text": "<div>連鎖店</div>- 網路評價：3.9"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "政治大學校門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.150838565799595,
+        "pitch": -0.21591671660691958,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.010911315626392692,
+          "pitch": 0.047364137630086844,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.8840446243736366,
+          "pitch": 0.05878249872259822,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.981215680901556,
+          "pitch": 0.0028480889536020015,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3599462741663064,
+          "pitch": -0.5242922013624991,
+          "title": "國立政治大學",
+          "text": "本校自民國 16 年創立，民國 43 年在臺復校，期間雖經改制、遷校，但始終秉持和諧自主、均衡、卓越之創校宗旨，不斷精進教學及學術研究。以期配合國家建設、社會發展的需要，培育優秀人才。"
+        },
+        {
+          "yaw": -0.9201133706119506,
+          "pitch": -0.12329666890152247,
+          "title": "文山指南郵局",
+          "text": "有 ATM<div>- 網路評價：4.4</div>"
+        },
+        {
+          "yaw": -0.2459193167268836,
+          "pitch": -0.11176761772185984,
+          "title": "身心健康中心",
+          "text": "<div>三樓是台北市立聯合醫院政大門診部</div>- 網路評價：3.0"
+        },
+        {
+          "yaw": -2.291509295326824,
+          "pitch": -0.013007210230476574,
+          "title": "政大一 公車站",
+          "text": "等公車..."
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "政治大學東側門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05357119124689724,
+          "pitch": 0.017603778721401397,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.1200289724853647,
+          "pitch": 0.012071005311685923,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7223046296361169,
+          "pitch": -0.08566416640715246,
+          "title": "國立政治大學",
+          "text": "本校自民國 16 年創立，民國 43 年在臺復校，期間雖經改制、遷校，但始終秉持和諧自主、均衡、卓越之創校宗旨，不斷精進教學及學術研究。以期配合國家建設、社會發展的需要，培育優秀人才。"
+        },
+        {
+          "yaw": -2.6766093208568797,
+          "pitch": -0.09001378914586233,
+          "title": "今日書局有限公司",
+          "text": "賣很多有的沒的<div>- 網路評價：4.5<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "指南路二段（往貓空）",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.873586819168022,
+          "pitch": 0.036368504310695826,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        },
+        {
+          "yaw": -3.1139824809119965,
+          "pitch": -0.008048084298110325,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9314961794353067,
+          "pitch": -0.10740935249601158,
+          "title": "滇味廚房",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.34862369025614,
+          "pitch": -0.12692941166667993,
+          "title": "LAZY PASTA",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "秀明路二段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.47745508738503695,
+          "pitch": -0.06697644528533964,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.6283949847626857,
+          "pitch": 0.05174296555086855,
+          "rotation": 1.5707963267948966,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1769234485830378,
+          "pitch": -0.42518522593047514,
+          "title": "圖書館 萬興分館",
+          "text": "可以借書<div>- 網路評價：4.2<br></div>"
+        },
+        {
+          "yaw": -2.5316144315112314,
+          "pitch": -0.025793607323940648,
+          "title": "萬興圖書館 公車站",
+          "text": "等公車..."
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "道南橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0336806787131234,
+          "pitch": -0.033283275098023424,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.0341927425862796,
+          "pitch": -0.09654308433681535,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.398108225280133,
+          "pitch": 0.05866815983618601,
+          "title": "小曼谷 泰國雲南料理政大店",
+          "text": "<div>泰式料理</div>- 網路評價：4.1"
+        }
+      ]
+    }
+  ],
+  "name": "政大",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ home/data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "政治大學校門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.150838565799595,
+        "pitch": -0.21591671660691958,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3599462741663064,
+          "pitch": -0.5242922013624991,
+          "title": "國立政治大學",
+          "text": "本校自民國 16 年創立，民國 43 年在臺復校，期間雖經改制、遷校，但始終秉持和諧自主、均衡、卓越之創校宗旨，不斷精進教學及學術研究。以期配合國家建設、社會發展的需要，培育優秀人才。"
+        },
+        {
+          "yaw": -0.9201133706119506,
+          "pitch": -0.12329666890152247,
+          "title": "文山指南郵局",
+          "text": "有 ATM<div>- 網路評價：4.4</div>"
+        },
+        {
+          "yaw": -0.2459193167268836,
+          "pitch": -0.11176761772185984,
+          "title": "身心健康中心",
+          "text": "<div>三樓是台北市立聯合醫院政大門診部</div>- 網路評價：3.0"
+        },
+        {
+          "yaw": -2.291509295326824,
+          "pitch": -0.013007210230476574,
+          "title": "政大一 公車站",
+          "text": "等公車..."
+        }
+      ]
+    }
+  ],
+  "name": "政大",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
